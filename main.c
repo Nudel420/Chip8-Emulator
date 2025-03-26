51ec52a0784ff4e73eac8e489dce59de98ce93cb
Elias Laptop <elias.hoebart@gmail.com>
2025-03-26 10:14:43 +0100

    init commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned short opcode; // opcodes each 2 bytes long
+unsigned char memory[4096]; // 4K RAM
+unsigned char V[16]; // general purpose registers ranging from V1 to VE
+unsigned short I; // Index register
+unsigned short pc; // program counter from 0x000 to 0xFFF
+
+/*
+    0x000-0x1FF -> Chip8 interpreter (contains font set in emu)
+    0x050-0x0A0 -> Used for the built in 4x5 pixel font set (0-F)
+    0x200-0xFFF -> Program ROM and free RAM
+  */
+
+
+
+int main (void){
+    printf("Hello World!\n");
+    return 0;
+}
